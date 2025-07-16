5cb945eee5c18917e4dbafa8bb945cb41079a8b5
Alexandre Santos <alexandre.santos@inovartel.net>
2025-07-16 17:45:51 -0300

    feat: Configurar deploy automático com GitHub Actions e Firebase Hosting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +12,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
