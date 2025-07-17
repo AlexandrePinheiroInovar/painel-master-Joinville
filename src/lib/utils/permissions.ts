c2b3e3523bc00449e06ca4baac1cc7a654761088
Alexandre Santos <alexandre.santos@inovartel.net>
2025-07-17 12:39:17 -0300

    feat(permissao): adiciona UID r9ZPLbtU91QplA5v1PeATPzD1ml2 aos módulos de Gestão de Motos e Venda de Motos

--- src/lib/utils/permissions.ts
+++ src/lib/utils/permissions.ts
@@ -5,6 +5,7 @@ export const ALLOWED_MOTORCYCLE_USER_IDS = [
   "edsTZ2zG54Ph2ZoNSyFZXoJj74s2",
   "FOHbVCbMyhadO3tm1rVdknwLVPr1",
   "870qQuK1lrcuOnWf1kUDOJOF8cv2", // Novo UID autorizado
+  "r9ZPLbtU91QplA5v1PeATPzD1ml2", // Novo UID autorizado
   // Adicione aqui outros UIDs permitidos
 ];
 
@@ -13,6 +14,7 @@ export const ALLOWED_VENDA_MOTOS_USER_IDS = [
   "edsTZ2zG54Ph2ZoNSyFZXoJj74s2",
   "FOHbVCbMyhadO3tm1rVdknwLVPr1",
   "870qQuK1lrcuOnWf1kUDOJOF8cv2", // Novo UID autorizado
+  "r9ZPLbtU91QplA5v1PeATPzD1ml2", // Novo UID autorizado
   // Adicione aqui outros UIDs permitidos
 ];
 
